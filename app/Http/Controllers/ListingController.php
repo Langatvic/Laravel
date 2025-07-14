fcbdc9d87ae25ec6bcb72b44b3c9d72545b1a23f
Langatvic <victorlangat6229@gmail.com>
2025-07-14 15:45:59 +0300

    its about laravel

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Listing;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    //show all listings
+    public function index(){
+      
+        return view('listings.index', [
+        'listings' => Listing::latest()->filter
+        (request(['tag', 'search']))->simplePaginate(2)
+    ]);
+    }
+    //show single listing
+    public function show(Listing $listing) {
+        return view('listings.show', [
+         'listing' => $listing
+    ]);
+
+    }
+    //show create form
+ public function create() {
+    return view('listings.create');
+ }
+
+    //store listing data
+ public function store(Request $request) {
+    $formFields = $request->validate([
+        'title' => 'required',
+        'company' => ['required', Rule::unique('listings', 'company')],
+        'location' => 'required',
+        'email' => ['required', 'email'],
+        'website' => 'required',
+        'tags' => 'required',
+        'description' => 'required',
+    ]);
+
+    //handle file upload
+    if($request->hasFile('logo')) {
+        $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+    }
+    $formFields['user_id'] = auth()->id();
+    
+    Listing::create($formFields);
+    return redirect('/')->with('message', 'Listing created successfully!');
+ }
+
+    //show edit form
+ public function edit(Listing $listing) {
+    
+    return view('listings.edit', [
+        'listing' => $listing
+    ]);
+ }
+   //update listing
+    public function update(Request $request, Listing $listing) {
+      
+      //make sure logged in  user is owner
+      if($listing->user_id != auth()->id()) {
+         abort(403, 'Unauthorized Action');
+      }
+
+         $formFields = $request->validate([
+        'title' => 'required',
+        'company' => ['required'],
+        'location' => 'required',
+        'email' => ['required', 'email'],
+        'website' => 'required',
+        'tags' => 'required',
+        'description' => 'required',
+    ]);
+
+      //handle file upload
+    if($request->hasFile('logo')) {
+        $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+    }
+    
+    $listing->update($formFields);
+    return back()->with('message', 'Listing updated successfully!');
+ }
+
+    //delete listing
+ public function destroy(Listing $listing) {
+    //make sure logged in  user is owner
+      if($listing->user_id != auth()->id()) {
+         abort(403, 'Unauthorized Action');
+      }
+
+
+   
+    $listing->delete();
+    return redirect('/')->with('message', 'Listing deleted successfully!');
+
+ }
+      //manage listings
+   public function manage() {
+    return view('listings.manage', [
+        'listings' => auth()->user()->listings()->get()
+    ]);
+   }
+}
+
+
+
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    //show Register/ Create Form
+    public function create() {
+        return view('users.register');
+    }
+    //Create New User
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'name' => 'required', 'min:5',
+            'email' => 'required', 'email', Rule::unique('users', 'email'),
+            'password' => 'required', 'min:6', 'confirmed'
+        ]);
+        //Hash Password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        //Create User
+        $user = User::create($formFields);
+
+        //Login User
+        auth()->login($user);
+        return redirect('/')->with('message', 'User created and logged in');
+    }
+    //Logout User
+    public function logout(Request $request) {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/')->with('message', 'You have been logged out');
+    }
+    //Login Form
+    public function login() {
+        return view('users.login');
+    }
+
+    //Login In User
+    public function authenticate(Request $request) {
+        $formFields = $request->validate([
+           
+            'email' => 'required', 'email',
+            'password' => 'required',
+        ]);
+        if (auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'You are now logged in');
+        }
+        return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
+    }
+        
+}
